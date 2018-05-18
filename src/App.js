02a442113bfccc74e02d1f42172b32a01e573e79
Soumya <soumyacool2012@gmail.com>
2018-05-18 10:21:07 +0530

    Added state to App.js and made default video search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import YTSearch from "youtube-api-search";
 import "./App.css";
-import API_KEY from "./config_keys";
 import Searchbar from "./components/Searchbar";
+import API_KEY from "./config_keys";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: "surfboards" }, data => {
+      this.setState({ videos: data });
+    });
+  }
+
   render() {
     return (
       <div className="App">
